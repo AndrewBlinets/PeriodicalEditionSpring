e1db8f574d4ecac5dbd0358da73d0215fe377161
Andrew <andreiblinets@gmail.com>
2017-05-24 21:54:51 +0300

    update MyQuery

--- src/main/java/by/andreiblinets/dao/constant/MyQuery.java
+++ src/main/java/by/andreiblinets/dao/constant/MyQuery.java
@@ -7,6 +7,6 @@ public final class MyQuery {
     public static final String GET_ALL_PAYMENT =  "from Payment";
     public static final String GET_ALL_CAMEL_CASE =  "from CamelCase";
     public static final String GET_ALL_SUBSCRIPTION =  "from Subscription";
-    public static final String GET_USER_BY_LOGIN_AND_PASSWORD =  "select * from Account where login = ? and password =? ";
-    public static final String CHEKING_LOGIN =  "select * from Account where login = ?";
+    public static final String GET_USER_BY_LOGIN_AND_PASSWORD =  "select * from Account where login = :login and password = :password ";
+    public static final String CHEKING_LOGIN =  "select * from Account where login = :login";
 }
